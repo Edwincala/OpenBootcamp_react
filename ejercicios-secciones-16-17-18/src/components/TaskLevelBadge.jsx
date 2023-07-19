88d42cee2e181ab74a0d3e71ceebde284b845900
Edwincala <86900578+Edwincala@users.noreply.github.com>
2023-07-19 18:46:23 -0500

    Removed extra logic

--- ejercicios-secciones-16-17-18/src/components/TaskLevelBadge.jsx
+++ ejercicios-secciones-16-17-18/src/components/TaskLevelBadge.jsx
@@ -1,20 +1,11 @@
 import { Badge, Container } from "@mui/material";
 import PropTypes from "prop-types";
-import LEVELS from "../models/level.enum";
 import Task from "../models/task.class";
 
 const TaskLevelBadge = ({ task }) => {
     return (
         <Container>
-            {LEVELS.NORMAL && (
-                <Badge color="primary" badgeContent={task.level}></Badge>
-            )}
-            {LEVELS.URGENT && (
-                <Badge color="primary" badgeContent={task.level}></Badge>
-            )}
-            {LEVELS.BLOCKING && (
-                <Badge color="primary" badgeContent={task.level}></Badge>
-            )}
+            <Badge color="primary" badgeContent={task.level}></Badge>
         </Container>
     );
 };
